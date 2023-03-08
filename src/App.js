755e8ef9ee7ad568c309c2f79a5594394b6ee497
Skyler Rasmussen <skylerrasmussen@Skylers-Air.lan>
2023-03-07 20:24:36 -1000

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route, Link, NavLink } from 'react-router-dom';
 
-function App() {
+// page imports
+import Home from './pages/Home';
+import About from './pages/About';
+import Projects from './pages/Projects';
+import Contact from './pages/Contact';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <header>
+        <nav>
+            <h1>Skyras13 Software</h1>
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="about">About Me</NavLink>
+            <NavLink to="projects">My Projects</NavLink>
+            <NavLink to="contact">Contact Me</NavLink>
+        </nav>
+    </header>
+        <main>
+            <Routes>
+                <Route path="/" element={<Home />} />;
+                <Route path="about" element={<About />} />;
+                <Route path="projects" element={<Projects />} />;
+                <Route path="contact" element={<Contact />} />;
+            </Routes>
+        </main>
+    </BrowserRouter>
   );
 }
 
-export default App;
+export default App
